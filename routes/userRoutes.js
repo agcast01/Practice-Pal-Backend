adf7d4c68dced0a3152154fbe730702beecbfe4f
Alex Castillo <108693776+agcast01@users.noreply.github.com>
2024-02-25 12:58:03 -0500

    added ability to sign up and login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,19 +5,33 @@ const bcrypt = require('bcrypt')
 const { User } = require('../db/models')
 
 router.get('/', (req, res) => {
-  console.log("beep")
   User.findAll()
     .then(res => JSON.stringify(res, null, 2))
     .then(json => res.send(json))
 })
 
-router.post('/', async (req, res) => {
+router.post('/signup', async (req, res) => {
   const newUser = req.body;
   newUser.createdAt = new Date();
   newUser.updatedAt = new Date();
   newUser.password = await bcrypt.hash(newUser.password, 10);
   await User.create(newUser);
   res.status(201).send('Success');
+})
+
+router.post('/login', async (req, res) => {
+  const attempt = req.body;
+  try {
+    const user = await User.findOne({where: {email: attempt.email}});
+    if (!user) throw new Error("No user matching that email and password combination.")
+    const passwordValidation = await bcrypt.compare(attempt.password, user.password);
+    if (!passwordValidation) throw new Error("No user matching that email and password combination.")
+    res.send(user.email)
+  } catch (e) {
+    res.send(e.message)
+    return
+  }
+  
 })
   
 
